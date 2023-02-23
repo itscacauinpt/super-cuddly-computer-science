9c7570434954c273a603b4520295046d1a2f2ad5
cacau <anaclaudia.sza@outlook.com>
2023-02-23 00:57:31 -0300

    feat: stack with java

---
+++ third_semester/data_structure/stack/dealing_with_stacks.java
@@ -0,0 +1,54 @@
+// situação-problema: desenvolvendo um algoritmo que recebe do user cinco int números
+// numa pilha com capacidade para cinco numeros e imprime esses numeros
+package stack;
+
+public class dealing_with_stacks {
+  int topo, tamanho;
+  int[] vetor;
+
+  public boolean PilhaVazia() {
+    if (topo == -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  public boolean PilhaCheia() {
+    if (topo >= tamanho-1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  public void Empilhar(int Elemento) {
+    if (!PilhaCheia()) {
+      topo += 1;
+      vetor[topo] = Elemento;
+    } else {
+      System.out.println("Pilha Cheia");
+    }
+  };
+
+  public int Desempilhar() {
+    int  desempilhado = 0;
+
+    if (PilhaVazia()) {
+      System.out.println("Pilha Vazia");
+    } else {
+      desempilhado = vetor[topo];
+      topo -= 1;
+    }
+
+    return desempilhado;
+  };
+
+  public void ElementoTopo() {
+    if (topo >= 0) {
+      System.out.println("Elemento do topo: " + vetor[topo]);
+    } else {
+      System.out.println("Pilha vazia");
+    }
+  };
+}
